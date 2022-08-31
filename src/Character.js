4b4b6858dc4ef4dbb1cb54aa9ed7994ca1df6519
Ömer Kor <60571310+omerKor@users.noreply.github.com>
2022-08-31 11:20:39 +0300

    first update

---
+++ src/Character.js
@@ -0,0 +1,4 @@
+export const numbers = '0123456789'
+export const upperCaseLetters = 'ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVWXYZ'
+export const lowerCaseLetters = 'abcçdefgğhıijklmnoÖpqrsştuüvwxyz'
+export const specialCharacters = "!'^+%&/()=?_#$½§{[]}|;:>÷`<.*-@é"
